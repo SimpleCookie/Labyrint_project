fbaa2b5a5796088dd32ff811c258842374764f1b
Johannes Lindén <pj-linden@hotmail.com>
2013-07-03 08:58:57 +0000

    This is a game based on the Maze klass in the master branch

--- Inputs.cpp
+++ Inputs.cpp
@@ -1,16 +1,9 @@
 #include <iostream>
 #include "Inputs.h"
 #include <string>
-int Check_valid_size(std::string value){
-	int size;
-	// If it cannot be converted to an int, return false
-	if (!(size = std::stoi(value)))
-		return 0;
-	// If it's a negative number and less than 3, convert it to positive
-	if (size < -3)
-		size *= -1;
+int Check_valid_size(unsigned int size){
 	// If size is in the intervall [3, -3], return false
-	if (size < 3 && size > -3)
+	if (size <= 3)
 		return 0;
 	return size;
 }
